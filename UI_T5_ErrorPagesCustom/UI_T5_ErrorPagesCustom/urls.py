a8a61e1de2550136d4eebc7075a7194f1d80b90c
Jonathan Alberto Barrera <jonabarreraabc@gmail.com>
2026-01-15 09:43:49 -0600

    errorPagesCustom finished

--- UI_T5_ErrorPagesCustom/UI_T5_ErrorPagesCustom/urls.py
+++ UI_T5_ErrorPagesCustom/UI_T5_ErrorPagesCustom/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from core import views as core
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', core.index,name='index'),
+    path('', core.index, name='index'),
 ]
+
+
