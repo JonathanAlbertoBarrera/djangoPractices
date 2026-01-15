382ec802b723203ffbcef903becf4c7d39ac990b
Jonathan Alberto Barrera <jonabarreraabc@gmail.com>
2026-01-14 23:30:41 -0600

    [add] error pages project started

---
+++ ErrorPages/ErrorPages/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from core import views as core
+
+urlpatterns = [
+    path('', core.index, name='index'),
+    path('contacto/', core.contacto, name='contacto'),
+    path('onepage/', core.onepage, name='onepage'),
+]
---
+++ ErrorPages/core/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, "core/index.html")
+
+def contacto(request):
+    print("Log el usuario entro al contacto")
+    return render(request, "core/contacto.html")
+
+def onepage(request):
+    return render(request, "core/onepage.html")
